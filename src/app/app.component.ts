1418cd2b286e55ee0cac0495d7facf802f28b75a
shahbhumi53 <shahbhumi53@gmail.com>
2019-12-10 14:11:02 +0530

    grid design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-state-management';
+  title = 'Angular State Management';
 }
